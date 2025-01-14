a8b96c66db9e1594c7e5c373efbcd2652c2c778a
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2025-01-13 22:24:15 -0300

    inclusion de boton sidebar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterModule} from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterModule} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, RouterLink, RouterLinkActive],
+  imports: [RouterModule, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  // Controla la visibilidad del botón de Sidebar
+  showSidebarButton = signal(false);
 
+  constructor(private router: Router, private sidebarService: SidebarService) {}
+
+  ngOnInit(): void {
+    // Actualiza la visibilidad del botón al inicializar y al cambiar la ruta
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateSidebarButton();
+      }
+    });
+
+    this.updateSidebarButton(); // Llama una vez al inicializar
+  }
+
+  private updateSidebarButton(): void {
+    this.showSidebarButton.set(this.router.url.startsWith('/doc'));
+  }
+
+  toggleSidebar(): void {
+    this.sidebarService.toggleSidebar(); // Alterna la visibilidad usando el servicio
+  }
 }
