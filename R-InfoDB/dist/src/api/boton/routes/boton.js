205caac0735f8aa679e2e8e689fd4803720c43b9
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2025-01-19 00:06:11 -0300

    Add files via upload

---
+++ R-InfoDB/dist/src/api/boton/routes/boton.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * boton router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::boton.boton');
---
+++ R-InfoDB/dist/src/api/documentacion/services/documentacion.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * documentacion service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::documentacion.documentacion');
