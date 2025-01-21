6d98e6e19e09a80dbee8af2f73eb35b53151f3f3
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2025-01-21 01:19:54 -0300

    CREACION Y ACTUALIZACION DE CONTENIDOS

---
+++ R-InfoDB/dist/src/api/subcontent-document/controllers/subcontent-document.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * subcontent-document controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::subcontent-document.subcontent-document');
